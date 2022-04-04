285fd5b070ea3a801f267a3c628e17cc1a91d4ac
AidenLeeeee <hoo8799@naver.com>
2022-04-04 12:48:27 +0900

    Extend Routes with Blueprint

--- project/__init__.py
+++ project/__init__.py
@@ -13,49 +13,14 @@ def create_app():
     
     if app.config['DEBUG']:
         app.config['SEND_FILE_MAX_AGE_DEFAULT'] = None
+        
+    ''' Routes INIT '''
+    from project.routes import base_route, auth_route
+    app.register_blueprint(base_route.bp)
+    app.register_blueprint(auth_route.bp)
     
-    '''CSRF INIT'''
+    ''' CSRF INIT '''
     csrf.init_app(app)
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    from project.forms.auth_form import LoginForm, RegisterForm
-    @app.route('/auth/login', methods=['GET', 'POST'])
-    def login():
-        form = LoginForm()
-        
-        # POST, validate OK!
-        if form.validate_on_submit():
-            user_id = form.data.get('user_id')
-            password = form.data.get('password')
-            return f'{user_id} {password}'
-        else:
-            pass
-            
-        return render_template('login.html', form=form)
-        
-    @app.route('/auth/register', methods=['GET', 'POST'])
-    def register():
-        form = RegisterForm()
-        
-        # POST, validate OK!
-        if form.validate_on_submit():
-            user_id = form.data.get('user_id')
-            user_name = form.data.get('user_name')
-            password = form.data.get('password')
-            repassword = form.data.get('repassword')
-            return f'{user_id} {user_name} {password} {repassword}'
-            
-        else:
-            pass
-        
-        return render_template('register.html', form=form)
-        
-    @app.route('/auth/logout')
-    def logout():
-        return 'logout'
     
     @app.errorhandler(404)
     def page_404(error):
