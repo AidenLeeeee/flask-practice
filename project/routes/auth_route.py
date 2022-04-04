586998371587f9af16d64c6f68a28379d97e75f4
AidenLeeeee <hoo8799@naver.com>
2022-04-04 16:11:58 +0900

    Integrate Errors with Flask Flash

--- project/routes/auth_route.py
+++ project/routes/auth_route.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 from project.forms.auth_form import LoginForm, RegisterForm
 
 NAME = 'auth'
 
 bp = Blueprint(NAME, __name__, url_prefix='/auth')
+
+@bp.route('/')
+def index():
+    return redirect(url_for(f'{NAME}.login'))
+    
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -15,7 +20,7 @@ def login():
         password = form.data.get('password')
         return f'{user_id} {password}'
     else:
-        pass
+        flash_form_errors(form)
         
     return render_template(f'/{NAME}/login.html', form=form)
         
@@ -32,10 +37,16 @@ def register():
         return f'{user_id} {user_name} {password} {repassword}'
         
     else:
-        pass
+        flash_form_errors(form)
     
     return render_template(f'{NAME}/register.html', form=form)
     
 @bp.route('/logout')
 def logout():
     return 'logout'
+
+def flash_form_errors(form):
+    for _, errors in form.errors.items():
+        for e in errors:
+            flash(e)
+    
