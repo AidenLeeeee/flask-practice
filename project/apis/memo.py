29e8f6cb74ed35c2402684243f399bada0f2f5a9
AidenLeeeee <hoo8799@naver.com>
2022-04-11 12:39:09 +0900

    Added Post Memo API

--- project/apis/memo.py
+++ project/apis/memo.py
@@ -1,6 +1,7 @@
+from email import parser
 from project.models.memo import Memo as MemoModel
 from project.models.user import User as UserModel
-from flask_restx import Namespace, fields, Resource
+from flask_restx import Namespace, fields, Resource, reqparse
 from flask import g
 
 ns = Namespace(
@@ -17,6 +18,10 @@ memo = ns.model('Memo', {
     'updated_at': fields.DateTime(description='memo_updated'),
 })
 
+parser = reqparse.RequestParser()
+parser.add_argument('title', required=True, help='title', location='form')
+parser.add_argument('content', required=True, help='content', location='form')
+
 
 @ns.route('')
 class MemoList(Resource):
@@ -32,6 +37,20 @@ class MemoList(Resource):
             MemoModel.created_at.desc()
         ).limit(10).all()
         return data
+    
+    @ns.expect(parser)
+    @ns.marshal_list_with(memo, skip_none=True)
+    def post(self):
+        '''Create Memo'''
+        args = parser.parse_args()
+        memo = MemoModel(
+            title=args['title'],
+            content = args['content'],
+            user_id = g.user.id
+        )
+        g.db.add(memo)
+        g.db.commit()
+        return memo, 201
     
 
 @ns.route('/<int:id>')
