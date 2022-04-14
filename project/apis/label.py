182951908c93829c066bb6bfe9fa5b043c789a61
AidenLeeeee <hoo8799@naver.com>
2022-04-15 00:56:00 +0900

    Added POST Label API

--- project/apis/label.py
+++ project/apis/label.py
@@ -1,5 +1,7 @@
-from flask_restx import Namespace, fields
+from flask_restx import Namespace, fields, reqparse, Resource
+from flask import g
 from project.models.labels import Label as LabelModel
+from project.models.user import User as UserModel
 
 ns = Namespace(
     'labels',
@@ -12,3 +14,35 @@ label = ns.model('Label', {
     'content': fields.String(required=True, description="label_content"),
     'created_at': fields.DateTime(description="label_created")
 })
+
+parser = reqparse.RequestParser()
+parser.add_argument('content', required=True, type=str, help='label_content', location='form')
+
+@ns.route('')
+class LabelList(Resource):
+    @ns.expect(parser)
+    @ns.marshal_list_with(label, skip_none=True)
+    def post(self):
+        '''Create Label'''
+        args = parser.parse_args()
+        content = args['content']
+        label = LabelModel.query.join(
+            UserModel,
+            UserModel.id == LabelModel.user_id
+        ).filter(
+            UserModel.id == g.user.id,
+            LabelModel.content == content
+        ).first()
+        
+        if label:
+            ns.abort(409)
+        
+        label = LabelModel(
+            content=content,
+            user_id=g.user.id
+        )
+        
+        g.db.add(label)
+        g.db.commit()
+        
+        return label, 201
